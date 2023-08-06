f7e6ac8d54b447b247a7838d8a20a1bf6449c85a
ingto <sign_int@163.com>
2023-08-06 21:41:21 +0800

    Update index.tsx

--- src/components/Bubble/index.tsx
+++ src/components/Bubble/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
-export interface BubbleProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'content'> {
+// export interface BubbleProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'content'> {
+//   type?: string;
+//   content?: React.ReactNode;
+// }
+
+// export const Bubble = React.forwardRef<HTMLDivElement, BubbleProps>((props, ref) => {
+//   const { type = 'text', content, children, ...other } = props;
+//   return (
+//     <div className={`Bubble ${type}`} data-type={type} ref={ref} {...other}>
+//       {content && <p>{content}</p>}
+//       {children}
+//     </div>
+//   );
+// });
+
+// import React from 'react';
+
+export interface BubbleProps extends React.HTMLAttributes<HTMLDivElement> {
   type?: string;
-  content?: React.ReactNode;
+  this_content?: React.ReactNode;
 }
 
 export const Bubble = React.forwardRef<HTMLDivElement, BubbleProps>((props, ref) => {
